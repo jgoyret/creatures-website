c55d2c29b93c4450697cc685d473447dd36ad495
jgoyret <j.goyret1475@gmail.com>
2022-10-26 16:19:02 -0300

    changing texture bicho

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ scene.background = new THREE.Color('rgb(5,5,5)')
 /**
  * Sizes
  */
- const sizes = {
+const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
@@ -25,60 +25,63 @@ scene.background = new THREE.Color('rgb(5,5,5)')
 /**
  * Camera
  */
- const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
- camera.position.set(0 , 6,-6)
- scene.add(camera)
- 
- /**
-  * Renderer
-  */
- const renderer = new THREE.WebGLRenderer({
-     canvas: canvas,
- })
- renderer.setSize(sizes.width, sizes.height)
- renderer.toneMapping = 0
- renderer.shadowMap.enabled = true
- renderer.physicallyCorrectLights = true
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.set(0, 6, -6)
+scene.add(camera)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.toneMapping = 0
+renderer.shadowMap.enabled = true
+renderer.physicallyCorrectLights = true
 
 
- /**
-  *  Lights
-  */
+/**
+ *  Lights
+ */
 
-  const directionalLight = new THREE.DirectionalLight( 0xffffff, 2 );
-  scene.add( directionalLight );
+const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
+scene.add(directionalLight);
+
+const light = new THREE.AmbientLight(0xffffff, 3); // soft white light
+scene.add(light);
 
 
- /**
-  * 3D BICHO
-  */
+/**
+ * 3D BICHO
+ */
 
 const bichox_loader = new GLTFLoader()
 
-bichox_loader.load('../static/bichox.glb', (glb) => { 
+bichox_loader.load('../static/bichox.glb', (glb) => {
     scene.add(glb.scene)
 },
-function ( xhr ) {
+    function (xhr) {
 
-   console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
 
-},
+    },
 
-function ( error ) {
+    function (error) {
 
-   console.log( 'An error happened' );
+        console.log('An error happened');
 
-})
+    })
 
 
- /**
-  * CONTROLS
-  */
+/**
+ * CONTROLS
+ */
 
- const controls = new OrbitControls(camera, renderer.domElement)
- controls.enableDamping = true
- controls.autoRotate = true
- controls.autoRotateSpeed = 0.3
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+controls.autoRotate = true
+controls.autoRotateSpeed = 0.3
 
 var animate = () => {
 
